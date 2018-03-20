525cc9d5b72d4de16e53236b043031682ed040ba
Yvan da Silva <yvan@yakika.com>
2018-03-20 23:54:03 +0100

    Adds JSDOC and Jest testing for package Renames the package for to use scope

--- src/index.js
+++ src/index.js
@@ -1,22 +1,56 @@
-/*
- * TODO: JSDOC, JEST, add other used pieces of code here
+/**
+ * Check if the promise is of Promise type
+ * @param {*} promise
+ * @return {boolean}
  */
-
-const func = promise => {
-  return promise.then(res => [undefined, res]).catch(err => [err, undefined])
+const _isPromise = promise => {
+  return promise && typeof promise.then === 'function'
 }
 
-
-/*
- * TODO: Improve the idea of a catch combinator
+/**
+ * Checks if obj is a function, curtosy of underscorejs.
+ * @param {*} obj
+ * @return {boolean}
  */
-const translateError = msg => {
-  const newErr = new Error(msg)
-  return oldErr => {
-    newErr.oldError = oldErr
-    throw newError
+const _isFunction = obj => {
+  return !!(obj && obj.constructor && obj.call && obj.apply)
+}
+
+/**
+ * Returns the [res, err] of an async function
+ * Normal data and sync functions are allowed to be passed through
+ * @param {*} promise
+ * @returns {Promise} Promise object represents [res, err]
+ */
+const func = promise => {
+  if (_isFunction(promise)) {
+    try {
+      return func(promise())
+    } catch (err) {
+      return [undefined, err]
+    }
+  }
+  if (!_isPromise(promise)) {
+    return [promise, undefined]
+  }
+
+  return promise.then(res => [res, undefined]).catch(err => [undefined, err])
+}
+
+/**
+ * Transforms an Error with a readable msg.
+ * @param {string} msg
+ * @return {trErr~inner} an err transform function
+ */
+const trErr = msg => {
+  /**
+   * Constructs new error
+   * @param {object} a standard error
+   * @throws {object} a standard error `error:${msg}\n- original error:${err}`
+   */
+  return err => {
+    throw new Error(`error: ${msg}\n- original error: ${err}`)
   }
 }
 
-
-// Support commonJS, AMD, etc. for npmjs
+module.exports = { func, trErr }
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,8 +1,122 @@
 const assert = require('assert')
-const func = require('../src/index')
+const { func, trErr } = require('../src/index')
+
+/**
+ * Testing : func
+ */
+const expectedText = 'my-value'
+const rejectedText = 'is-rejected'
+const thrownText = 'is-thrown'
 
 describe('Synchronous error handling', () => {
-    it('return "my-value"', ()=> {
-        // TODO: Do testing on all tools
+  it(`res is equal to ${expectedText} and err is undefined`, async () => {
+    const [res, err] = await func(expectedText)
+    expect(res).toEqual(expectedText)
+    expect(err).toBeUndefined()
+  })
+
+  it(`res is undefined, err is undefined and error was caught`, async () => {
+    let [res, err] = [undefined, undefined]
+    try {
+      const fnThrow = () => {
+        throw new Error(thrownText)
+      }
+      ;[res, err] = await func(fnThrow())
+    } catch (e) {
+      expect(e.message).toEqual(thrownText)
+    }
+    expect(res).toBeUndefined()
+    expect(err).toBeUndefined()
+  })
+
+  it(`res is undefined and err.message is equal to ${thrownText}
+    even without a proper function call`, async () => {
+    const [res, err] = await func(() => {
+      throw new Error(thrownText)
     })
+    expect(res).toBeUndefined()
+    expect(err.message).toEqual(thrownText)
+  })
+})
+
+describe('Asynchronous error handling', () => {
+  const asyncTask = (text, isRejected, isThrown) => {
+    return new Promise((resolve, reject) => {
+      if (isThrown) throw new Error(thrownText)
+      if (isRejected) reject(new Error(rejectedText))
+      setTimeout(() => {
+        resolve(text)
+      }, 10)
+    })
+  }
+  const testAwait = async msg => {
+    return await asyncTask(msg, false, false)
+  }
+  const testAwaitReject = async msg => {
+    return await asyncTask(msg, true, false)
+  }
+  const testAwaitThrow = async msg => {
+    return await asyncTask(msg, false, true)
+  }
+
+  it(`res is equal to ${expectedText} and err is undefined`, async () => {
+    const [res, err] = await func(testAwait(expectedText))
+    expect(res).toEqual(expectedText)
+    expect(err).toBeUndefined()
+  })
+
+  it(`res is undefined and err.message is equal to ${rejectedText}`, async () => {
+    const [res, err] = await func(testAwaitReject(expectedText))
+    expect(res).toBeUndefined()
+    expect(err.message).toEqual(rejectedText)
+  })
+
+  it(`res is undefined and err.message is equal to ${thrownText}`, async () => {
+    const [res, err] = await func(testAwaitThrow(expectedText))
+    expect(res).toBeUndefined()
+    expect(err.message).toEqual(thrownText)
+  })
+})
+
+/**
+ * Testing : trErr
+ */
+
+describe('Asynchronous transform error to something readable', () => {
+  const prom = () => {
+    return new Promise((resolve, reject) => {
+      reject(new Error())
+    })
+  }
+
+  it(`Error is now custom and the original error was kept`, () => {
+    const msg = 'human readable err'
+    let errMsg, originalErr
+    prom()
+      .catch(trErr(msg))
+      .catch(e => {
+        expect(errMsg).toEqual(
+          `error: ${msg}\n- original error: ${originalErr}`
+        )
+      })
+  })
+})
+describe('Synchronous transform error to something readable', () => {
+  it(`Error is now custom and the original error was kept`, () => {
+    const msg = 'human readable err'
+    let originalErr
+    // External try/catch for testing behavior
+    try {
+      try {
+      } catch (e) {
+        // translate some sync error that we don't want to catch
+        originalErr = e
+        trErr(msg)(e)
+      }
+    } catch (err) {
+      expect(err).toEqual(
+        `error: ${msg}\n- original error: ${originalErr}`
+      )
+    }
+  })
 })
